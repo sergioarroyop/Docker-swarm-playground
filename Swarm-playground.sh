ea3ceae4666599b9ac02b37e2bf0d9daf413acc2
Sergio Arroyo <sergioarroyopay@gmail.com>
2018-11-13 21:35:06 +0100

    First functions

--- Swarm-playground.sh
+++ Swarm-playground.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-# Running machines
-for num in {1..3} do {
-    docker-machine create mach$num
+playground-init(){
+    if [ -z $1 ]
+    then
+        echo "Please insert a valid number of nodes"
+        exit 1
+    fi
+
+    # Checking machines and turning them up them
+    for num in {1..$1} do {
+        docker-machine create bro$num
+    }
+
+    # Getting master IP
+    ip_master=$(docker-machine ip bro1)
+
+    echo $ip_master
+
+    ## Starting swarm
+    #$(eval docker-machine env bro1)"
+    #
+    #docker swarm init --advertise-addr $ip_master"
+    #
+    ## Getting master and worker token
+    #master_token=$(docker-machine ssh bro1 "docker swarm join-token master")
+    #worker_token=$(docker-machine ssh bro1 "docker swarm join-token worker")
 }
 
-# Getting master IP
-ip_master=$(docker-machine ip mach1)
+playground-delete(){
+    # Deleting playground
+    for del in $(docker-machine ls | grep bro[0-9]) do {
+        docker-machine rm $del
+    }
+}
 
-# Starting swarm
-docker-machine ssh mach1 "docker swarm init --advertise-addr $ip_master"
-
-# Getting master and worker token
-
-master_token=$(docker-machine ssh mach1 "docker swarm join-token master")
-worker_token=$(docker-machine ssh mach1 "docker swarm join-token worker")
 
