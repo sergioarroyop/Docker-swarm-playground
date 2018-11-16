0adae29065af6ca627661fb572a35873042233e4
Sergio <sergioarroyopay@gmail.com>
2018-11-16 14:37:12 +0100

    Bug in line 23

--- Swarm-playground.sh
+++ Swarm-playground.sh
@@ -1,37 +1,80 @@
 #!/bin/bash
 
-playground-init(){
-    if [ -z $1 ]
-    then
-        echo "Please insert a valid number of nodes"
-        exit 1
-    fi
+playground-init() {
+
+    ints='^[0-9]+$'
+
+    #if ! [[ $1 =~ $re ]]
+    #then
+    #    echo "Please insert a valid number of nodes"
+    #    exit 1
+    #fi
 
     # Checking machines and turning them up them
-    for num in {1..$1} do {
-        docker-machine create bro$num
-    }
+    for ((num=1; num<=$1; num++)); do {
+        echo "Creating machine bro$num"
+        docker-machine create --driver virtualbox bro$num
+    } done
 
     # Getting master IP
     ip_master=$(docker-machine ip bro1)
 
-    echo $ip_master
+    # Starting swarm
+    $(eval docker-machine env bro1)
+    
+    docker swarm init --advertise-addr $ip_master
+    
+    # Getting master and worker token
+    master_token=$(docker-machine ssh bro1 "docker swarm join-token master -q")
+    worker_token=$(docker-machine ssh bro2 "docker swarm join-token worker -q")
 
-    ## Starting swarm
-    #$(eval docker-machine env bro1)"
-    #
-    #docker swarm init --advertise-addr $ip_master"
-    #
-    ## Getting master and worker token
-    #master_token=$(docker-machine ssh bro1 "docker swarm join-token master")
-    #worker_token=$(docker-machine ssh bro1 "docker swarm join-token worker")
+    # Joining workers to swarm
+    for ((num=2; num<=$1; num++)); do {
+        echo "Joining bro$num as a worker"
+        eval $(docker-machine env bro$num)
+        docker swarm join --token worker_token $ip_master:2377
+    } done
 }
 
 playground-delete(){
     # Deleting playground
-    for del in $(docker-machine ls | grep bro[0-9]) do {
+    for del in $(docker-machine ls | grep -o bro'[0-9$]'); do {
+        echo "Deleting machine $del"
         docker-machine rm $del
-    }
+    } done
 }
 
+init(){
+    echo "What do you want to do?"
+    echo "1. Create playground"
+    echo "2. Delete playground"
+    echo "3. Exit"
+    read option
 
+    clear
+
+    if [ $option == 1 ]
+    then
+        echo "How many nodes do you want?"
+        read num
+    fi
+}
+
+init
+
+case $option in
+    1)
+        playground-init $num
+    ;;
+    2)
+        playground-delete
+    ;;
+    3)
+        echo "Bye Bye"
+        exit 0
+    ;;
+    *)
+        echo "Please, select an option"
+        init        
+    ;;
+esac
